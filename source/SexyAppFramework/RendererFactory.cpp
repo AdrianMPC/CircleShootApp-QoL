777b839981b46cc7b87ceac501aad7e28ffcf800
AdrianMPC <adrian.pinado@usil.pe>
2025-08-30 20:05:16 -0500

    doesn't compile right now

--- source/SexyAppFramework/RendererFactory.cpp
+++ source/SexyAppFramework/RendererFactory.cpp
@@ -2,7 +2,9 @@
 #include "D3DInterface.h"            
 std::unique_ptr<RendererInterface> CreateD3D11Renderer(); 
 
-std::unique_ptr<RendererInterface> CreateRenderer(bool useD3D11) {
-    if (useD3D11) return CreateD3D11Renderer();
+std::unique_ptr<RendererInterface> CreateRenderer(bool useD3D11) { // temporal xdxxdxd
+    if (useD3D11) {
+        return CreateD3D11Renderer();
+    }
     return std::make_unique<Sexy::D3DInterface>();
 }
